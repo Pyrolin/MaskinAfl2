6ac77894c1b0a8f0acc0ee7cdabab0d4cd2cf7e0
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-04-28 10:24:16 +0200

    Made print to txt file

---
+++ Save.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+int main()
+{
+    FILE *f = fopen("C:/Users/Madsh/OneDrive/Skrivebord/Ny mappe/Test/kort.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    const char *text = "TestTest";
+    fprintf(f, "Ekstra test: %s\n", text);
+    
+    fclose(f);
+    return(0);
+}
