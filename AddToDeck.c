3e80c808281a2b6d624a27a177f780aee1dcc6de
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-05-05 14:16:17 +0200

    Fixed add to deck

--- AddToDeck.c
+++ AddToDeck.c
@@ -6,19 +6,20 @@
  * @param newCard The card that should be added to the deck
  * @param deck The current last card in the deck, NULL if none
  */
-Card AddToDeck(Card *newCard, Card **deck) {
+Card AddToDeck(Card *newCard, Card *deck) {
     Card *previous = NULL;
-    Card *current = *deck;
 
-    while (current != NULL) {
-        previous = current;
-        current = current->next;
+    if (deck != NULL) {
+        Card *current = deck;
+        while (current != NULL) {
+            previous = current;
+            current = current->next;
+        }
+
     }
 
     newCard->next = NULL;
-    if (previous == NULL) {
-        *deck = newCard;
-    } else {
+    if (deck != NULL) {
         previous->next = newCard;
     }
     return *newCard;
