78f4a5e8a2be31a533bc350bd94af73cf35246d2
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-05-05 14:45:10 +0200

    Updated split and shuffle

--- Split.c
+++ Split.c
@@ -5,21 +5,56 @@
 #include "AddToDeck.c"
 #define MAX_CARDS 52
 
-void shuffle(int deck[], int size, int split){
-    if (split <= 0 split >= size){
-        srand(time(NULL));
-        split =rand() % (size -1) +1;
+void shuffleDeck(Card **card1, Card **card2) {
+Card *lastNode = card1;
+while (lastNode->next != NULL) {
+    lastNode = lastNode->next;
+}
+lastNode->next = *card2;
+
+int length = 0;
+        for (Card *node = card1; node != NULL; node = node->next) {
+            length++;
+        }
+        for (Card *node1 = card1; node1 != NULL; node1 = node1->next) {
+            int randIndex = rand() % length;
+            Card *node2 = card1;
+            for (int i = 0; i < randIndex; i++) {
+                node2 = node2->next;
+            }
+            Card temp = *node1;
+            *node1 = *node2;
+            *node2 = temp;
+        }
+}
+
+void splitAndShuffleDeck(Card *card, int split) {
+    Card *deck1 = NULL;
+    Card *deck2 = NULL;
+
+    int count =1;
+    while (card != NULL) {
+        Card *newNode = (Card*)malloc(sizeof(Card));
+        newNode->value = card->value;
+        newNode->suit = card->suit;
+        newNode->next = NULL;
+
+        if (count <= split) {
+            AddToDeck(newNode, &deck1);
+        } else {
+            AddToDeck(newNode, &deck2);
+        }
+        count++;
+        card = card->next;
     }
-    // den splitter kort deckne til to deck.
-    int pile1[split];
-    int pile2[size-split];
-    for (int i = 0; i <split; i++){
-        pile1[i] = deck[i];
-    }
-    for (int i = split; i <size; i++){
-        pile2[i - split] = deck[i];
+    shuffleDeck(deck1, deck2);
+
+    for (Card *node = deck1; node != NULL; node =node->next){
+        printf("%c of %c>'\n", node->value, node->suit);
     }
 }
+
+
 
 void splitDeck(Card *card, int split) {
 // list points to the head of a linked list of Flights
