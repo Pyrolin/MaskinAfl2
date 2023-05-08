0447f7b276791e0b21dbe588549bff06b881c96b
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-05-08 16:51:48 +0200

    Move cards part 1

--- moveCards.c
+++ moveCards.c
@@ -1,3 +1,42 @@
+#include <string.h>
 void moveCards(char command[]) {
-    
+    char srcCard[2];
+    char destColumn[2];
+    char srcColoumn[2];
+    strncpy(destColumn,&command[6],2);
+    strncpy(srcCard,&command[2],2);
+    strncpy(srcColoumn,command,2);
+    if(strcmp(srcColoumn,"c1")){
+
+    }
+    if(strcmp(srcColoumn,"c2")){
+
+    }
+    if(strcmp(srcColoumn,"c3")){
+
+    }
+    if(strcmp(srcColoumn,"c4")){
+
+    }
+    if(strcmp(srcColoumn,"c5")){
+
+    }
+    if(strcmp(srcColoumn,"c6")){
+
+    }
+    if(strcmp(srcColoumn,"c7")){
+
+    }
+    if(strcmp(srcColoumn,"f1")){
+
+    }
+    if(strcmp(srcColoumn,"f2")){
+
+    }
+    if(strcmp(srcColoumn,"f3")){
+
+    }
+    if(strcmp(srcColoumn,"f4")){
+
+    }
 }
