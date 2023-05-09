ee87a972152a2c4aa508cdf2724dd3dc3c08e1ed
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-05-09 13:27:25 +0200

    Create split.h

---
+++ split.h
@@ -0,0 +1,16 @@
+//
+// Created by Mads Hauberg on 09-05-2023.
+//
+
+#ifndef __SPLIT_H__
+#define __SPLIT_H__
+
+#include "linkedlists.h"
+
+void shuffleDeck(Card *card1, Card *card2);
+
+void splitAndShuffleDeck(Card *card, int split);
+
+void splitDeck(Card *card, int split);
+
+#endif
