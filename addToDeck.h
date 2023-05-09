bf6d1d05de12944c89755aa10988abaf2e8c013a
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-05-09 13:19:15 +0200

    Create addToDeck.h

---
+++ addToDeck.h
@@ -0,0 +1,12 @@
+//
+// Created by Mads Hauberg on 09-05-2023.
+//
+
+#ifndef __ADDTODECK_H__
+#define __ADDTODECK_H__
+
+#include "linkedlists.h"
+
+Card* AddToDeck(Card *newCard, Card *card);
+
+#endif
