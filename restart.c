9a8b1eca81b2883936aa56498c4c158cda29981c
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-05-09 13:27:06 +0200

    changed deck to card

--- restart.c
+++ restart.c
@@ -1,21 +1,23 @@
 #include <stdlib.h>
 #include <string.h>
-#include "Main.h"
 
- void quitGame(char* deck){
+ void quitGame(char* card){
 
-     free(deck);
+    extern char PHASE[];
+
+     free(card);
 
      // return to startup phase.
      strcpy(PHASE,"STARTUP");
+
 }
 
- void playGame(){
+ void playGame() {
     // initialize the deck of cards
-     char* deck = malloc(sizeof(char)* 52 );
+     char* card = malloc(sizeof(char)* 52 );
      // play the game using the deck of cards.
 
 
      // if the player wants to quit the game and return to the startup phase.
-     quitGame(deck);
+     quitGame(card);
 }
