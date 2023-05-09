6e98ce8fb65e4585016dbd587b2f8d8ab9d4a09b
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2023-05-09 13:20:04 +0200

    Update command.c

--- command.c
+++ command.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-void startPhase(char input[]) {
-       if(strcmp(input, "LD") != 0) {
-           //Load();
-       }
+#include "play.h"
+#include "load.h"
+#include "main.h"
 
-       if(strcmp(input, "SC") != 0){
-           //showCards();
-       }
-       if(strcmp(input, "SD") != 0){
-           //splitDeck();
-       }
-       if(strcmp(input, "SH") != 0){
-           //shuffleDeck();
-       }
-       if(strcmp(input, "S") != 0){
-           //Save();
-       }
-       if(strcmp(input, "QQ") != 0){
-           //quit();
-       }
-       else {
-           printf("Command not available in the STARTUP phase.");
-       }
+void startPhase(char input[]) {
+    if(strcmp(input, "LD") == 0) {
+        load();
+    } else if(strcmp(input, "SC") == 0){
+        //showCards();
+    } else if(strcmp(input, "SD") == 0){
+        //splitDeck();
+    } else if(strcmp(input, "SH") == 0){
+        //shuffleDeck();
+    } else if(strcmp(input, "S") == 0){
+        //Save();
+    } else if(strcmp(input, "QQ") == 0){
+        //quit();
+    } else if (strcmp(input, "P") == 0) {
+        Play();
+    } else {
+        extern char message[];
+        strcpy(message, "Command not available in the STARTUP phase.");
     }
+}
+
+
 
 void playPhase(char input[]) {
     if (strcmp(input, "Q") != 0){
