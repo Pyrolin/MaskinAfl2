69bad23bd82f1a280483d9c58f85a44e5e34266f
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2023-05-09 15:36:08 +0200

    getPreviousValue metode og getNextValue metode

--- moveCards.c
+++ moveCards.c
@@ -15,14 +15,93 @@ void moveFromTo(Card *card1, Card *card2) {
     }
 
 }
+char getPreviousValue(char value){
+    switch (value) {
+        case 'K':{
+            return 'Q';
+        }
+        case 'Q':{
+            return 'J';
+        }
+        case 'J':{
+            return 'T';
+        }
+        case 'T':{
+            return '9';
+        }
+        case '9':{
+            return '8';
+        }
+        case '8':{
+            return '7';
+        }
+        case '7':{
+            return '6';
+        }
+        case '6':{
+            return '5';
+        }
+        case '5':{
+            return '4';
+        }
+        case '4':{
+            return '3';
+        }
+        case '3':{
+            return '2';
+        }
+        case '2':{
+            return 'A';
+        }
+        case 'A':{
+            return 'X';
+        }
+        default:{
+            return 'X';
+        }
+    }
+}
 
 char getNextValue(char value) {
     switch (value) {
-        case 'a': {
+        case 'A': {
             return '2';
         }
         case '2': {
             return '3';
+        }
+        case '3':{
+            return '4';
+        }
+        case '4':{
+            return '5';
+        }
+        case '5': {
+            return '6';
+        }
+        case '6': {
+            return '7';
+        }
+        case '7':{
+            return '8';
+        }
+        case '8':{
+            return '9';
+        }
+        case '9':{
+            return 'T';
+        }
+        case 'T':{
+            return 'J';
+        }
+        case 'J':{
+            return 'Q';
+        }
+        case 'Q':{
+            return 'K';
+        }
+        case 'K':{
+            return 'X';
         }
         default: {
             return 'X';
